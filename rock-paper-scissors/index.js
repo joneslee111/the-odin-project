efbc1f5e14f4a701567bc6006c269eaad30281ea
Lee Jones <leejones111@hotmail.co.uk>
2022-01-29 23:30:07 +0000

    game now plays until the player or computer reaches 5

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,59 +1,69 @@
 function game() {
-    let playerScore = 0
-    let computerScore = 0
+    let playerScore = 0;
+    let computerScore = 0;
+    let maxScore = 4;
 
-    
+    for (i = 0; (playerScore || computerScore) <= maxScore; i++) {
     // player inputs their move
-    function playerSelection() {
-        let playerInput = prompt('Please pick Rock, Paper, or Scissors');
-        let playerMove = playerInput.toLowerCase();
-        return playerMove;
-    }
-    let playerChoice = playerSelection();
-    console.log(playerChoice);
-
-    // computer randomly selects from an array and returns the value
-    function computerPlay() {
-        const array = ['rock', 'paper', 'scissors'];
-        let move = array[Math.floor(Math.random() * array.length)];
-        return move;
-    }
-    let computerChoice = computerPlay();
-    console.log(computerChoice);
-    
-    
-    // game decides winner based on computers choice
-    function playRound(playerChoice, computerChoice) {
-        if (playerChoice === computerChoice) {
-            return 'tie'
-        } else if (playerChoice == 'rock') {
-            if (computerChoice == 'paper') {
-                return 'lose';
-            } else if (computerChoice == 'scissors') {
-                return 'win';
-            }
-        }  else if (playerChoice == 'paper') {
-            if (computerChoice == 'rock') {
-                return 'win';
-            } else if (computerChoice == 'scissors') {
-                return 'lose';
-            }
-        } else if (playerChoice == 'scissors') {
-            if (computerChoice == 'paper') {
-                return 'win';
-            } else if (computerChoice == 'rock') {
-                return 'lose';
-            }
-        } else {
-            return 'Players move is not valid';
+        function playerSelection() {
+            let playerInput = prompt('Please pick Rock, Paper, or Scissors. First to 5 wins.');
+            let playerMove = playerInput.toLowerCase();
+            return playerMove;
         }
-    }
-    
-    
-    
-    // console.log(playerSelection); // console.logging players selection of rock
-    console.log(playRound(playerChoice, computerChoice));
-    
+        let playerChoice = playerSelection();
+        console.log(playerChoice);
+
+        // computer randomly selects from an array and returns the value
+        function computerPlay() {
+            const array = ['rock', 'paper', 'scissors'];
+            let move = array[Math.floor(Math.random() * array.length)];
+            return move;
+        }
+        let computerChoice = computerPlay();
+        console.log(computerChoice);
+        
+        
+        // game decides winner based on computers choice
+        function playRound(playerChoice, computerChoice) {
+            if (playerChoice === computerChoice) {
+                return 'tie'
+            } else if (playerChoice == 'rock') {
+                if (computerChoice == 'paper') {
+                    computerScore++;
+                    return 'lose';
+                } else if (computerChoice == 'scissors') {
+                    playerScore++;
+                    return 'win';  
+                }
+
+            }  else if (playerChoice == 'paper') {
+                if (computerChoice == 'rock') {
+                    playerScore++;
+                    return 'win';
+                } else if (computerChoice == 'scissors') {
+                    computerScore++;
+                    return 'lose';
+                }
+
+            } else if (playerChoice == 'scissors') {
+                if (computerChoice == 'paper') {
+                    playerScore++;
+                    return 'win';
+                } else if (computerChoice == 'rock') {
+                    computerScore++;
+                    return 'lose';
+                }
+            } else {
+                return 'Players move is not valid';
+            }
+        }
+        playRound();
+        console.log(playRound(playerChoice, computerChoice));
+
+
+}
+        console.log(playerScore);
+        console.log(computerScore);
 }
 
 game();
