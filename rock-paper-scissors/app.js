daeec4e2a167ecf7fefd451ca97271507ebd1333
Lee Jones <leejones111@hotmail.co.uk>
2022-02-08 11:53:01 +0000

    now updating score after every move

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,4 +1,3 @@
-
 let maxRound = 4;
 let playerScore = 0;
 let computerScore = 0;
@@ -17,12 +16,11 @@ buttons.forEach((button) => {
     
     playerChoice = button.id;
     playRound(playerChoice, computerChoice);
-    
   });
 });
 
-  // computer randomly selects from an array and returns the value
- function computerSelection() {
+// computer randomly selects from an array and returns the value
+function computerSelection() {
   const array = ['rock', 'paper', 'scissors'];
   let move = array[Math.floor(Math.random() * array.length)];
   return move;
@@ -30,70 +28,92 @@ buttons.forEach((button) => {
 
 
 
-  // game decides winner based on computers choice
-  function playRound(playerChoice, computerChoice) {
-    computerChoice = computerSelection();
-    console.log(playerChoice);
-    console.log(computerChoice);
+// game decides winner based on computers choice
+function playRound(playerChoice, computerChoice) {
+  computerChoice = computerSelection();
+  console.log(playerChoice);
+  console.log(computerChoice);
+  
+  if (playerChoice === computerChoice) {
+    maxRound++;
+    roundNum++;
     console.log(playerScore);
     console.log(computerScore);
-    if (playerChoice === computerChoice) {
-      maxRound++;
+    console.log('tie');
+    return 'tie';
+  } else if (playerChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      computerScore++;
       roundNum++;
-      return 'tie';
-    } else if (playerChoice === 'rock') {
-      if (computerChoice === 'paper') {
-        computerScore++;
-        roundNum++;
-        return 'lose';
-      } else if (computerChoice === 'scissors') {
-        playerScore++;
-        roundNum++;
-        return 'win';  
-      }
-    }  else if (playerChoice ==='paper') {
-      if (computerChoice ==='rock') {
-        playerScore++;
-        roundNum++;
-        return 'win';
-      } else if (computerChoice === 'scissors') {
-        computerScore++;
-        roundNum++;
-        return 'lose';
-      }
-    } else if (playerChoice === 'scissors') {
-      if (computerChoice === 'paper') {
-        playerScore++;
-        roundNum++;
-        return 'win';
-      } else if (computerChoice === 'rock') {
-        computerScore++;
-        roundNum++;
-        return 'lose';
-      }
-    } else {
-      maxRound++;
+      console.log(playerScore);
+  console.log(computerScore);
+      console.log('lose');
+      return 'lose';
+    } else if (computerChoice === 'scissors') {
+      playerScore++;
       roundNum++;
-      return 'Players move is not valid';
+      console.log(playerScore);
+  console.log(computerScore);
+      console.log('win');
+      return 'win';  
     }
+  }  else if (playerChoice ==='paper') {
+    if (computerChoice ==='rock') {
+      playerScore++;
+      roundNum++;
+      console.log(playerScore);
+  console.log(computerScore);
+      console.log('win');
+      return 'win';
+    } else if (computerChoice === 'scissors') {
+      computerScore++;
+      roundNum++;
+      console.log(playerScore);
+  console.log(computerScore);
+      console.log('lose');
+      return 'lose';
+    }
+  } else if (playerChoice === 'scissors') {
+    if (computerChoice === 'paper') {
+      playerScore++;
+      roundNum++;
+      console.log(playerScore);
+  console.log(computerScore);
+      console.log('win');
+      return 'win';
+    } else if (computerChoice === 'rock') {
+      computerScore++;
+      roundNum++;
+      console.log(playerScore);
+  console.log(computerScore);
+      console.log('lose');
+      return 'lose';
+    }
+  } else {
+    maxRound++;
+    roundNum++;
+    return 'Players move is not valid';
+  };
   endGame(playerScore, computerScore);
-  }
-  
 
-  // reveals winner based on score at the end of the last round
-  function endGame(playerScore, computerScore) {
-    if (roundNum === 5) {
-      if (computerScore > playerScore) {
-        return 'Computer Wins';
-      } else if (playerScore > computerScore) {
-        return 'Player Wins';
-      } else {
-        return "It's a tie!";
-      }
-    } else {
-      return 'Keep Playing!';
-    }
-  }
+};
+
+
+// reveals winner based on score at the end of the last round
+function endGame(playerScore, computerScore) {
+
+if (roundNum === 5) {
+  if (computerScore > playerScore) {
+    return 'Computer Wins';
+  } else if (playerScore > computerScore) {
+    return 'Player Wins';
+  } else {
+    return "It's a tie!";
+  };
+} else {
+  return 'Keep Playing!';
+};
+};
 
    
 
