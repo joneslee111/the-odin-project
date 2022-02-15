e5697472b39967bb5eac9b3124c55fd95e68f385
Lee Jones <leejones111@hotmail.co.uk>
2022-02-15 23:46:58 +0000

    created the 16x16 layout

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,2 +1,9 @@
 const container = document.querySelector('#container');
-container.textContent = 'This is a test';
+// container.textContent = 'This is a test';
+
+
+for (let i = 0; i < 256; i++) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    container.appendChild(grid);
+};
