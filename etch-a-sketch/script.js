14fbb3c49df308481f379e2cb83111322606a87f
Lee Jones <leejones111@hotmail.co.uk>
2022-02-16 00:20:36 +0000

    createGrid creates grid to the amount the player inputs

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,9 +1,24 @@
 const container = document.querySelector('#container');
 // container.textContent = 'This is a test';
+let gridCount = prompt('How many squares would you like?');
+console.log(gridCount);
 
 
-for (let i = 0; i < 256; i++) {
+
+function createGrid() {
+  for (let i = 0; i < gridCount; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
     container.appendChild(grid);
+  };
 };
+
+createGrid();
+
+// for (let rows = 0; rows < 16; rows++) {
+//     for (let cols = 0; cols < 16; cols++) {
+//       const grid = document.createElement('div');
+//       grid.classList.add('grid');
+//       container.appendChild(grid);
+//     };
+// };
