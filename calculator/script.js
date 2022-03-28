34deba71c754de1667ceac2be38b3216033793c9
Lee Jones <leejones111@hotmail.co.uk>
2022-03-28 19:17:18 +0100

    not functional yet

--- calculator/script.js
+++ calculator/script.js
@@ -1,16 +1,20 @@
 function add(a, b) {
-  return a + b;
+  console.log(a + b);
+  answer = a + b;
 };
 
 function subtract(a, b) {
+  console.log(a-b);
   return a - b;
 };
 
 function multiply(a, b) {
+  console.log(a*b);
   return a * b;
 };
 
 function divide(a, b) {
+  console.log(a/b);
   return a / b;
 }
 
@@ -25,27 +29,64 @@ function operate(operator, a, b) {
     return divide(a, b);
   };
 };
-let display = 0;
+let a = [];
+let b = [];
+let operator = '';
+let answer = 0;
 
-let buttonSelect = document.querySelectorAll('.btn');
-buttonSelect.forEach((btn) => {
-  btn.addEventListener('click', whichButton)
-});
+// listens for button presses for the numbers 
+// and calls whichButton function
+let buttonSelect = document.querySelector('.btn');
+// buttonSelect.addEventListener('click', whichButton);
+// let buttonSelect = document.querySelectorAll('.btn');
+// for (let i = 0; i < buttonSelect.length; i++) {
+//   buttonSelect[i].addEventListener('click', whichButton);
+// };
 
+
+// pass the value of the button thats pressed to the display array
+// then calls populateDisplay, whilst passing the display arr
 function whichButton(button) {
-  display += button;
-  // console.log(display);
-  populateDisplay(display);
+  let screenContainer = document.querySelector('.calc-screen');
+
+  if (operator == '+' ) {
+    screenContainer.textContent = '';
+    b += button;
+    screenContainer.textContent = b;
+  } else {
+    a += button;
+    screenContainer.textContent = a;
+  }
+    console.log(a);
+    console.log(b);
+  // populateDisplay(a, b);
 };
 
-// populate display
-function populateDisplay(display) {
-  let screenContainer = document.querySelector('.calc-screen');
-  // let screenText = screenContainer.querySelector('.calc-text');
-  screenContainer.textContent = display;
-  // console.log(display);
+// takes in the display arg and does some dom manipulation
+// selects the calc-screen class and ands the display value as text
+// function populateDisplay(a, b) {
+//   let screenContainer = document.querySelector('.calc-screen');
+//   screenContainer.textContent = a;
+
+// };
+
+// 
+function whichOperator(button) {
+  // let a = 5;
+  // let b = 5;
+  operator = button;
 
 }
 
-
-// console.log(display);
+function solution() {
+  if (operator === '/') {
+    operate(divide, 5, 5);
+  } else if (operator === 'x') {
+    operate(multiply, 5, 5);
+  } else if (operator === '-') {
+    operate(subtract, 5, 5);
+  } else if (operator === '+') {
+    operate(add, a, b);
+  };
+// console.log(operator);
+}
